43221e8e5bcdefcc9c8f11bb4e86bfe2d5d43b7f
Madavoka <postbalaclava@gmail.com>
2016-02-01 17:26:04 +0200

    Improved layouts for the main amd detail fragments. Added a set of icons for the layouts and changed data format in the main fragment.

--- app/src/main/java/com/insomniac/sunshine/MainActivity.java
+++ app/src/main/java/com/insomniac/sunshine/MainActivity.java
@@ -23,11 +23,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
-                    .commit();
-        }
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
+//                    .commit();
+//        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
