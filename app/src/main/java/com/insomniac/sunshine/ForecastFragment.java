81f5f0281f4b19841aa96f48c933b9b17f7bae11
Madavoka <postbalaclava@gmail.com>
2016-02-04 19:08:42 +0200

    Replaced FetchWeatherTask with a service (SunshineService). Changed updateWeather method in th the ForecastFragment.

--- app/src/main/java/com/insomniac/sunshine/ForecastFragment.java
+++ app/src/main/java/com/insomniac/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.insomniac.sunshine;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.insomniac.sunshine.data.WeatherContract;
+import com.insomniac.sunshine.service.SunshineService;
 
 
 /**
@@ -169,9 +171,10 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
 
     private void updateWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
+                Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
     @Override
